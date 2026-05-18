96f745944a5e24b91eb3517bf15c54e4fc0ba652
“zhutao” <2512323438@qq.com>
2026-05-18 15:19:49 +0800

    add two scooterPhoneID

--- urbanova/src/main/java/com/lcyhz/urbanova/domain/ScooterPhoneID.java
+++ urbanova/src/main/java/com/lcyhz/urbanova/domain/ScooterPhoneID.java
@@ -9,6 +9,8 @@ public class ScooterPhoneID {
 
     static {
         DEVICE_TO_SCOOTER.put("7c7e01ba-c5e8-4865-aa15-16c40cc7abef", "SCO-0001");
+        DEVICE_TO_SCOOTER.put("0a2a92de-7892-45df-8e4a-46284d615845", "SCO-0002");
+        DEVICE_TO_SCOOTER.put("1a8a6725-cc09-4571-aae2-efd339894240", "SCO-0003");
     }
 
     public static String getScooterId(String phoneDeviceId) {
